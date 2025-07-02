ff36346f2cc221643745832d16f7e6ef2d3c8314
jackworths <93794839+jackworths@users.noreply.github.com>
2025-07-02 15:27:52 +0700

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    double a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Sum: " << (a + b) << endl;
+    cout << "Difference: " << (a - b) << endl;
+
+    return 0;
+}
