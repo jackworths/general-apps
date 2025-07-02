57acc27a47367f6698f602961f4c84e7dfd91edf
jackworths <93794839+jackworths@users.noreply.github.com>
2025-07-02 15:43:04 +0700

    Update guessing_game.cpp

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
+int main() {
+    srand(time(0));
+    int secretNumber = rand() % 100 + 1; // random 1-100
+    int guess;
+    int attempts = 0;
+
+    cout << "Welcome to Number Guessing Game!" << endl;
+    cout << "I have picked a number between 1 and 100. Try to guess it!" << endl;
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess > secretNumber) {
+            cout << "Too high! Try again." << endl;
+        } else if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl;
+        } else {
+            cout << "Congratulations! You found the number in " << attempts << " tries." << endl;
+        }
+    } while (guess != secretNumber);
+
+    return 0;
+}
